6b7b0fe5137155c9f09cca371a19876dfef39601
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-12 00:38:26 -0500

    definicion e implementacion del metodo para listar a los representantes legales

--- src/main/java/com/isai/demoregistrationsystem/app/service/ILegalRepresentative.java
+++ src/main/java/com/isai/demoregistrationsystem/app/service/ILegalRepresentative.java
@@ -1,7 +1,11 @@
 package com.isai.demoregistrationsystem.app.service;
 
 import com.isai.demoregistrationsystem.app.models.entities.LegalRepresentative;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ILegalRepresentative {
     void saveLegalRepresentative(LegalRepresentative legalRepresentative);
+
+    Page<LegalRepresentative> findAll(Pageable pageable);
 }
--- src/main/java/com/isai/demoregistrationsystem/app/service/impl/LegalRepresentativeService.java
+++ src/main/java/com/isai/demoregistrationsystem/app/service/impl/LegalRepresentativeService.java
@@ -4,6 +4,8 @@ import com.isai.demoregistrationsystem.app.models.entities.LegalRepresentative;
 import com.isai.demoregistrationsystem.app.repositorys.LegalRepresentativeRepository;
 import com.isai.demoregistrationsystem.app.service.ILegalRepresentative;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,5 +17,10 @@ public class LegalRepresentativeService
     @Override
     public void saveLegalRepresentative(LegalRepresentative legalRepresentative) {
         legalRepresentativeRepository.save(legalRepresentative);
+    }
+
+    @Override
+    public Page<LegalRepresentative> findAll(Pageable pageable) {
+        return legalRepresentativeRepository.findAll(pageable);
     }
 }
