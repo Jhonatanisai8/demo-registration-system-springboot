cfa42bdbb0fbe11a764f7867a50677566e215a1e
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-12 11:57:02 -0500

    renombrando el nombre de la tabla

--- src/main/java/com/isai/demoregistrationsystem/app/models/entities/LegalRepresentative.java
+++ src/main/java/com/isai/demoregistrationsystem/app/models/entities/LegalRepresentative.java
@@ -1,14 +1,13 @@
 package com.isai.demoregistrationsystem.app.models.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "segal_representative")
+@Table(name = "legal_representative")
 public class LegalRepresentative {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
