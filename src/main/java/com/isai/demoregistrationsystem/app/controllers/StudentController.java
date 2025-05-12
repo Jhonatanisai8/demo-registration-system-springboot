732b3870470b27eff67ba845e7a448c613b0b066
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-12 12:40:34 -0500

    implementacion del metodo para guardar un estudiante

--- src/main/java/com/isai/demoregistrationsystem/app/controllers/StudentController.java
+++ src/main/java/com/isai/demoregistrationsystem/app/controllers/StudentController.java
@@ -1,30 +1,42 @@
 package com.isai.demoregistrationsystem.app.controllers;
 
+import com.isai.demoregistrationsystem.app.editors.LegalRepresentativePropertiesEditors;
 import com.isai.demoregistrationsystem.app.models.entities.LegalRepresentative;
 import com.isai.demoregistrationsystem.app.models.entities.Student;
 import com.isai.demoregistrationsystem.app.service.impl.LegalRepresentativeService;
 import com.isai.demoregistrationsystem.app.service.impl.StudentService;
 import com.isai.demoregistrationsystem.app.utils.pagination.PageRender;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/students")
 @RequiredArgsConstructor
 public class StudentController {
+    private final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 
     private final StudentService studentService;
     private final LegalRepresentativeService legalRepresentativeService;
+    private final LegalRepresentativePropertiesEditors editorLegalRepresentative;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        //registramos el representante legal
+        binder.registerCustomEditor(LegalRepresentative.class, "legalRepresentative", editorLegalRepresentative);
+    }
 
     @GetMapping(path = "")
     public String showStudents(@RequestParam(name = "page", defaultValue = "0") int page,
@@ -39,8 +51,28 @@ public class StudentController {
     }
 
     @GetMapping(path = "/create")
-    public String viewCreateStudent() {
+    public String viewCreateStudent(Map<String, Object> model) {
+        Student student = new Student();
+        model.put("title", "Registro de Estudiantes");
+        model.put("student", student);
         return "students/create-student";
+    }
+
+    @PostMapping(path = "/create")
+    public String saveStudent(
+            @Valid Student student,
+            BindingResult result,
+            Model model) {
+        model.addAttribute("subtitle", "Registro de Estudiante");
+        LOGGER.info("DATOS DEL FROM: {}", student);
+        if (result.hasErrors()) {
+            model.addAttribute("title", "Registro de Estudiantes");
+            model.addAttribute("legalRepresentativeServices", legalRepresentativeServices());
+            return "students/create-student";
+        }
+        studentService.saveStudent(student);
+        System.out.println("SE GUARDO EL ESTUDIANTE");
+        return "redirect:/students";
     }
 
     @ModelAttribute("legalRepresentativeServices")
